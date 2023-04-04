629dd58a4702972c92fd62d694d970d6494ae0cd
Rubén Patiño <rpatino@clicoh.com>
2023-04-04 17:48:52 -0500

    update

---
+++ src/hooks/useCalculator.tsx
@@ -0,0 +1,149 @@
+import {useRef, useState} from 'react';
+
+export enum OperationsEnum {
+  sum,
+  res,
+  mul,
+  div,
+}
+
+export const useCalculator = () => {
+  const [numberOne, setNumberOne] = useState('0');
+  const [numberTwo, setNumberTwo] = useState('');
+
+  const operationRef = useRef<OperationsEnum>();
+
+  const signoRef = useRef('');
+
+  function addNumber(pressNumber: string) {
+    if (pressNumber === '.' && numberOne.includes('.')) {
+      return;
+    }
+    if (pressNumber === '0' && numberOne === '0') {
+      setNumberOne(pressNumber);
+    } else if (pressNumber === '.' && numberOne.startsWith('0')) {
+      setNumberOne('0.');
+    } else if (pressNumber !== '0' && numberOne === '0') {
+      setNumberOne(pressNumber);
+    } else {
+      setNumberOne(numberOne + pressNumber);
+    }
+  }
+
+  function operationCal(operationSelect: OperationsEnum) {
+    if (numberOne.endsWith('.')) {
+      return;
+    }
+    if (numberOne === '0') {
+      return;
+    }
+
+    if (numberOne !== '0' && numberTwo !== '') {
+      return;
+    }
+
+    operationRef.current = operationSelect;
+
+    switch (operationRef.current) {
+      case OperationsEnum.mul: {
+        signoRef.current = 'x';
+        break;
+      }
+      case OperationsEnum.div: {
+        signoRef.current = '/';
+        break;
+      }
+      case OperationsEnum.res: {
+        signoRef.current = '-';
+        break;
+      }
+      default: {
+        signoRef.current = '+';
+      }
+    }
+    setNumberTwo(numberOne);
+    setNumberOne('0');
+  }
+
+  function clearNumber() {
+    setNumberOne('0');
+    setNumberTwo('');
+    signoRef.current = '';
+  }
+
+  function changeType() {
+    if (numberOne === '0') {
+      return;
+    }
+    if (numberOne.includes('-')) {
+      setNumberOne(numberOne.replace('-', ''));
+    } else {
+      setNumberOne('-' + numberOne);
+    }
+  }
+
+  function delNumber() {
+    const cant = numberOne.length;
+    if (cant === 1) {
+      setNumberOne('0');
+    } else if (cant === 2 && numberOne.startsWith('-')) {
+      setNumberOne('0');
+    } else {
+      const newNumber = numberOne.substring(0, cant - 1);
+      setNumberOne(`${newNumber}`);
+    }
+  }
+
+  function resultCal() {
+    const one = Number.parseFloat(numberOne);
+    const two = Number.parseFloat(numberTwo);
+
+    if (signoRef.current === '') {
+      return;
+    }
+
+    switch (operationRef.current) {
+      case OperationsEnum.mul: {
+        const res = two * one;
+        setNumberOne(`${res}`);
+        break;
+      }
+      case OperationsEnum.div: {
+        if (one === 0) {
+          return;
+        }
+        const res = two / one;
+        setNumberOne(`${res}`);
+        break;
+      }
+      case OperationsEnum.res: {
+        const res = two - one;
+        setNumberOne(`${res}`);
+        break;
+      }
+      default: {
+        const res = two + one;
+        setNumberOne(`${res}`);
+        break;
+      }
+    }
+
+    setNumberTwo('');
+    signoRef.current = '';
+  }
+
+  return {
+    resultCal,
+    delNumber,
+    changeType,
+    clearNumber,
+    operationCal,
+    addNumber,
+    numberOne,
+    numberTwo,
+    setNumberOne,
+    setNumberTwo,
+    signoRef,
+    operationRef,
+  };
+};
--- src/screens/CalculadorScreen.tsx
+++ src/screens/CalculadorScreen.tsx
@@ -1,136 +1,21 @@
-import React, {useRef, useState} from 'react';
+import React from 'react';
 import {SafeAreaView, StatusBar, Text, View} from 'react-native';
 import {ButtonCalculadora} from '../components/ButtonCalculadora';
 import {styles} from '../theme/CalculadoraTheme';
-
-enum OperationsEnum {
-  sum,
-  res,
-  mul,
-  div,
-}
+import {OperationsEnum, useCalculator} from '../hooks/useCalculator';
 
 export const CalculadoraScreen = () => {
-  const [numberOne, setNumberOne] = useState('0');
-  const [numberTwo, setNumberTwo] = useState('');
-
-  const operationRef = useRef<OperationsEnum>();
-
-  const signo = useRef('');
-
-  function addNumber(pressNumber: string) {
-    if (pressNumber === '.' && numberOne.includes('.')) {
-      return;
-    }
-    if (pressNumber === '0' && numberOne === '0') {
-      setNumberOne(pressNumber);
-    } else if (pressNumber === '.' && numberOne.startsWith('0')) {
-      setNumberOne('0.');
-    } else if (pressNumber !== '0' && numberOne === '0') {
-      setNumberOne(pressNumber);
-    } else {
-      setNumberOne(numberOne + pressNumber);
-    }
-  }
-
-  function operationCal(operationSelect: OperationsEnum) {
-    if (numberOne.endsWith('.')) {
-      return;
-    }
-    if (numberOne === '0') {
-      return;
-    }
-
-    if (numberOne !== '0' && numberTwo !== '') {
-      return;
-    }
-
-    operationRef.current = operationSelect;
-
-    switch (operationRef.current) {
-      case OperationsEnum.mul: {
-        signo.current = 'x';
-        break;
-      }
-      case OperationsEnum.div: {
-        signo.current = '/';
-        break;
-      }
-      case OperationsEnum.res: {
-        signo.current = '-';
-        break;
-      }
-      default: {
-        signo.current = '+';
-      }
-    }
-    setNumberTwo(numberOne);
-    setNumberOne('0');
-  }
-
-  function clearNumber() {
-    setNumberOne('0');
-    setNumberTwo('');
-    signo.current = '';
-  }
-
-  function changeType() {
-    if (numberOne === '0') {
-      return;
-    }
-    if (numberOne.includes('-')) {
-      setNumberOne(numberOne.replace('-', ''));
-    } else {
-      setNumberOne('-' + numberOne);
-    }
-  }
-
-  function delNumber() {
-    const cant = numberOne.length;
-    if (cant === 1) {
-      setNumberOne('0');
-    } else if (cant === 2 && numberOne.startsWith('-')) {
-      setNumberOne('0');
-    } else {
-      const newNumber = numberOne.substring(0, cant - 1);
-      setNumberOne(`${newNumber}`);
-    }
-  }
-
-  function resultCal() {
-    const one = Number.parseFloat(numberOne);
-    const two = Number.parseFloat(numberTwo);
-
-    switch (operationRef.current) {
-      case OperationsEnum.mul: {
-        const res = two * one;
-        setNumberOne(`${res}`);
-        break;
-      }
-      case OperationsEnum.div: {
-        if (one === 0) {
-          return;
-        }
-        const res = two / one;
-        setNumberOne(`${res}`);
-        break;
-      }
-      case OperationsEnum.res: {
-        const res = two - one;
-        setNumberOne(`${res}`);
-        break;
-      }
-      default: {
-        const res = two + one;
-        setNumberOne(`${res}`);
-        break;
-      }
-    }
-
-    setNumberTwo('');
-    signo.current = '';
-  }
-
+  const {
+    resultCal,
+    addNumber,
+    changeType,
+    clearNumber,
+    delNumber,
+    operationCal,
+    numberOne,
+    numberTwo,
+    signoRef,
+  } = useCalculator();
   return (
     <SafeAreaView style={styles.containerGlobal}>
       <StatusBar backgroundColor="black" barStyle="light-content" />
@@ -142,7 +27,7 @@ export const CalculadoraScreen = () => {
             adjustsFontSizeToFit={true}>
             {numberTwo}
           </Text>
-          <Text style={styles.textSigno}>{signo.current}</Text>
+          <Text style={styles.textSigno}>{signoRef.current}</Text>
         </View>
         <Text
           style={styles.textResultOne}
